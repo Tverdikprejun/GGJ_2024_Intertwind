5458d5f2d44e7c3d43c57a3dd254993818309f72
slava300 <slava300@gmail.com>
2024-01-25 23:33:42 +0200

    add a outlet and cable and a script

---
+++ Assets/Scripts/outletscript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outletscript : MonoBehaviour
+{
+    bool slotOfOutlet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slotOfOutlet = true;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
