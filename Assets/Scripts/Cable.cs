30b351035a9637a160abfc7abb70accb2313d051
Tverdikprejun <miroslav.tverdovskii@gmail.com>
2024-01-26 01:17:43 +0200

    added drag and drop test build(not working)

--- Assets/Scripts/Cable.cs
+++ Assets/Scripts/Cable.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Cable : MonoBehaviour
+public class Cable : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDragHandler
 {
+    private RectTransform rectTransform;
+    private Image image;
 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
 }
