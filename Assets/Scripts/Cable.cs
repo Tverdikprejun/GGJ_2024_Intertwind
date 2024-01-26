2d7bc14ff12d649a643bdaa3e05c4c7b3d73b376
Tverdikprejun <miroslav.tverdovskii@gmail.com>
2024-01-26 14:16:43 +0200

    .

--- Assets/Scripts/Cable.cs
+++ Assets/Scripts/Cable.cs
@@ -15,7 +15,7 @@ public class Cable : MonoBehaviour
 
     private LineRenderer lineRenderer;
 
-    private Vector3[] tipsPositions;
+    private Vector3[] tipsPositions = new Vector3[2];
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
@@ -24,55 +24,7 @@ public class Cable : MonoBehaviour
     private void Update()
     {
         tipsPositions[0] = new Vector3(bottomTip.transform.position.x, bottomTip.transform.position.y, bottomTip.transform.position.z);
-        tipsPositions[1] = new Vector3(bottomTip.transform.position.x, bottomTip.transform.position.y, bottomTip.transform.position.z);
+        tipsPositions[1] = new Vector3(topTip.transform.position.x, topTip.transform.position.y, topTip.transform.position.z);
         lineRenderer.SetPositions(tipsPositions);
     }
-
-    //public void OnBeginDrag(PointerEventData eventData)
-    //{
-    //    image.color = new Color(255, 255, 255, 170);
-    //}
-
-    //public void OnDrag(PointerEventData eventData)
-    //{
-    //    rectTransform.anchoredPosition += eventData.delta;
-    //    transform.position = Input.mousePosition;
-    //}
-
-    //public void OnEndDrag(PointerEventData eventData)
-    //{
-    //    image.color = new Color(255, 255, 255, 170);
-    //}
-
-    //bool canMove;
-    //bool dragging;
-    //Collider2D collider;
-    //void Start()
-    //{
-    //    collider = GetComponent<Collider2D>();
-    //    canMove = false;
-    //    dragging = false;
-    //}
-    //void Update()
-    //{
-    //    Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        if (collider == Physics2D.OverlapPoint(mousePos))
-    //        {
-    //            canMove = true;
-    //        }
-    //        else
-    //        {
-    //            canMove = false;
-    //        }
-    //        if (canMove) { dragging = true; }
-    //    }
-    //    if(dragging) { transform.position = mousePos; }
-    //    if(Input.GetMouseButtonUp(0)) 
-    //    {
-    //        canMove = false;
-    //        dragging = false;
-    //    }
-    //}
 }
