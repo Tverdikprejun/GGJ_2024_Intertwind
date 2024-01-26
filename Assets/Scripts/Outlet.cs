6f97ba98b1c4f6aaf6f196c4b2ad5b11689f9183
Barbarakoroteev <barbarakoroteev@gmail.com>
2024-01-26 15:41:57 +0200

    WIP - outlet script

--- Assets/Scripts/Outlet.cs
+++ Assets/Scripts/Outlet.cs
@@ -3,4 +3,16 @@ using UnityEngine;
 public class Outlet : MonoBehaviour
 {
     private CableTip _currentCableTip = null;
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("WTF");
+
+        if (!other.TryGetComponent<CableTip>(out CableTip otherCableTip)) {  return; }
+
+        _currentCableTip = otherCableTip;
+
+        Debug.Log($"Cable tip found and updated! {_currentCableTip.name}");
+    }
 }
