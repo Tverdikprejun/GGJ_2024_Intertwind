1dcb7748ab679d85a04a3eab33d9223a96acf536
Barbarakoroteev <barbarakoroteev@gmail.com>
2024-01-26 17:07:05 +0200

    adede two spriets for connected no connected

--- Assets/Scripts/Outlet.cs
+++ Assets/Scripts/Outlet.cs
@@ -3,14 +3,34 @@ using UnityEngine;
 
 public class Outlet : MonoBehaviour
 {
+    [SerializeField]
+    private SpriteRenderer _renderer;
+
+    [SerializeField]
+    private Sprite occupiedSprite;
+
+    [SerializeField]
+    private Sprite nonOccupiedSprite;
+
     private CableTip _currentCableTip = null;
+
 
     public bool GetIsOccupied() => _currentCableTip != null;
 
     public void SetCableTip(CableTip cableTip)
     {
         _currentCableTip = cableTip;
+        ChangeSprite(true);
     }
 
-    public void ResetCableTip() { _currentCableTip = null; }
+    public void ResetCableTip() 
+    { 
+        _currentCableTip = null;
+        ChangeSprite(false);
+    }
+
+    private void ChangeSprite(bool isOccupied)
+    {
+        _renderer.sprite = isOccupied ? occupiedSprite : nonOccupiedSprite;
+    }
 }
