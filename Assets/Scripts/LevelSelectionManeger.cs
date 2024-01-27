342ecbd5c22996f22c6db19616cd50c82ede4aab
Tverdikprejun <miroslav.tverdovskii@gmail.com>
2024-01-27 11:16:49 +0200

    Level selection manager function added

--- Assets/Scripts/LevelSelectionManeger.cs
+++ Assets/Scripts/LevelSelectionManeger.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelectionManeger : MonoBehaviour
 {
-    int numberOfScnce;
-    int[] scenes = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-
-    public void SelectLevel()
+    public void LoadScene(int LevelIndex)
     {
-
+        SceneManager.LoadScene(LevelIndex);
     }
 }
